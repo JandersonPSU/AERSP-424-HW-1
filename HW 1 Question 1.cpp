70b368eae429f0237a6e41b16549866e5c26e8d0
JandersonPSU <jea5462@psu.edu>
2024-10-20 15:17:21 -0400

    Finished all componets of the code all that I need to do now is test and comment

--- HW 1 Question 1.cpp
+++ HW 1 Question 1.cpp
@@ -57,15 +57,10 @@ int main()
     cout << endl << "Please enter the baggage moment arm in inches:" << endl;
     cin >> BaggageMomentArm;
 
-//vector<double> FrontWeight(NumFrontPassengers);
-//vector<double> RearWeight(NumRearPassengers);
-
-
-
 double FrontWeightSum = 0;
 double RearWeightSum = 0;
 
-    for (int i = 1; i <= NumFrontPassengers; i++) 
+    for (int i = 1; i <= NumFrontPassengers; i++) // Used ChatGPT for this to find an error (I had i < NumFrontPassengers which resulted in me having 1 less than desired)
     {
         double FrontWeight = 0;
         cout << endl << "Please enter the weight for front seat occupant " << i << " in pounds: " << endl;
@@ -94,24 +89,88 @@ double RearWeightSum = 0;
     {
     
         if (WeightTO > MaxWeightTO)
-
-            cout << endl << "The current take off weight of the plane (" << fixed << setprecision(2) << WeightTO << ") pounds is above the max designed take off weight of " << MaxWeightTO << " pounds." << endl;
+        {
+            cout << endl << "The current take off weight of the plane (" << fixed << setprecision(2) << WeightTO << ") pounds is above the max designed take off weight of " << fixed << setprecision(2) << MaxWeightTO << " pounds." << endl;
             double FuelWeightRemove = (WeightTO - MaxWeightTO); // Pounds of fuel that need to be removed
             double FuelGallonsRemove = (WeightTO - MaxWeightTO) / FuelDenisty;
             WeightTO -= FuelWeightRemove;
             NetMoment = DryWeightMoment + (FrontWeightSum * FrontSeatMomentArm) + (RearWeightSum * RearSeatMomentArm) + ((FuelWeight - FuelWeightRemove) * FuelMomentArm) + (BaggageWeight * BaggageMomentArm);
             CGLocation = NetMoment / WeightTO; 
-            cout << "In order to make take off weight requirements (" << fixed << setprecision(2) << FuelWeightRemove << ") pounds of fuel was removed corrisponding to (" << fixed << setprecision(2) << FuelGallonsRemove << ") Gallons." << endl;
-            cout << "The newe take off weight of the plane is now (" << fixed << setprecision(2) << WeightTO << ") pounds." << endl;
+            cout << "In order to make take off weight requirements (" << fixed << setprecision(2) << FuelWeightRemove << ") pounds of fuel was removed which is (" << fixed << setprecision(2) << FuelGallonsRemove << ") Gallons." << endl;
+            cout << "The new take off weight of the plane is now (" << fixed << setprecision(2) << WeightTO << ") pounds." << endl;
+        }
 
         if (CGLocation < CGForwardLim)
+        {   
+            cout << endl << "The current location of the center of gravity (CG) of the plane (" << fixed << setprecision(2) << CGLocation << " inches) is ahead of the safe forward limit for the CG which is" 
+                 << fixed << setprecision(2)  << CGForwardLim << " inches." << endl;
+                
+            double NetMomentAdjustFCG = (CGForwardLim - CGLocation) * WeightTO;
+            double FuelWeightAjustFCG = NetMomentAdjustFCG / FuelMomentArm;
+            double FuelGallonAdjustFCG = FuelWeightAjustFCG / FuelDenisty;
+            NetMoment = DryWeightMoment + (FrontWeightSum * FrontSeatMomentArm) + (RearWeightSum * RearSeatMomentArm) + ((FuelWeight - FuelWeightAjustFCG) * FuelMomentArm) + (BaggageWeight * BaggageMomentArm);
+            CGLocation = NetMoment / WeightTO;
 
+            double FuelMomentArmCheckFCG = FuelMomentArm - CGForwardLim; 
+            
+            if (FuelMomentArmCheckFCG < 0) // Fuel needs to be removed
+            {
+                cout << "In order to make take off CG requirements (" << fixed << setprecision(2) << FuelWeightAjustFCG << ") pounds of fuel was removed which is (" << fixed << setprecision(2) << FuelGallonAdjustFCG << ") Gallons." << endl;
+                cout << "The new location of the CG for the plane is at (" << fixed << setprecision(2) << CGLocation << ") inches." << endl;
+                MaxFuel -= FuelGallonAdjustFCG;
+                WeightTO -= FuelWeightAjustFCG;
+            }
+
+            if (FuelMomentArmCheckFCG > 0) // Fuel needs to be added
+            {
+                cout << "In order to make take off CG requirements (" << fixed << setprecision(2) << FuelWeightAjustFCG << ") pounds of fuel was added which is (" << fixed << setprecision(2) << FuelGallonAdjustFCG << ") Gallons." << endl;
+                cout << "The new location of the CG for the plane is at (" << fixed << setprecision(2) << CGLocation << ") inches." << endl;
+                MaxFuel += FuelGallonAdjustFCG;
+                WeightTO += FuelWeightAjustFCG;
+            }
+        }
 
 
         if (CGLocation > CGRearLim)
+        {   
+            cout << endl << "The current location of the center of gravity (CG) of the plane (" << fixed << setprecision(2) << CGLocation << " inches) is behind of the safe rear limit for the CG which is" 
+                 << fixed << setprecision(2) << CGRearLim << " inches." << endl;
 
-        else
+            double NetMomentAdjustRCG = (CGLocation - CGRearLim) * WeightTO;
+            double FuelWeightAjustRCG = NetMomentAdjustRCG / FuelMomentArm;
+            double FuelGallonAdjustRCG = FuelWeightAjustRCG / FuelDenisty;
+            NetMoment = DryWeightMoment + (FrontWeightSum * FrontSeatMomentArm) + (RearWeightSum * RearSeatMomentArm) + ((FuelWeight - FuelWeightAjustRCG) * FuelMomentArm) + (BaggageWeight * BaggageMomentArm);
+            CGLocation = NetMoment / WeightTO;
+
+            double FuelMomentArmCheckRCG = CGRearLim - FuelMomentArm; 
+            
+            if (FuelMomentArmCheckRCG < 0) // Fuel needs to be removed
+            {
+                cout << "In order to make take off CG requirements (" << fixed << setprecision(2) << FuelWeightAjustRCG << ") pounds of fuel was removed which is (" << fixed << setprecision(2) << FuelGallonAdjustRCG << ") Gallons." << endl;
+                cout << "The new location of the CG for the plane is at (" << fixed << setprecision(2) << CGLocation << ") inches." << endl;
+                MaxFuel -= FuelGallonAdjustRCG;
+                WeightTO -= FuelWeightAjustRCG;
+            }
+
+            if (FuelMomentArmCheckRCG > 0) // Fuel needs to be added
+            {
+                cout << "In order to make take off CG requirements (" << fixed << setprecision(2) << FuelWeightAjustRCG << ") pounds of fuel was added which is (" << fixed << setprecision(2) << FuelGallonAdjustRCG << ") Gallons." << endl;
+                cout << "The new location of the CG for the plane is at (" << fixed << setprecision(2) << CGLocation << ") inches." << endl;
+                MaxFuel += FuelGallonAdjustRCG;
+                WeightTO += FuelWeightAjustRCG;
+            }
+        }
 
     }
+    else
+    {
+        cout << endl << "The current plane setup is within design limits" << endl;
+    }
+
+    cout << endl << "The final plane set up at take off is:" << endl;
+    cout << "The plane's takeoff weight is " << fixed << setprecision(2) <<  WeightTO << " pounds." << endl;
+    cout << "The plane's CG is located at " << fixed << setprecision(2) << CGLocation << " inches." << endl;
+    cout << "The plane is carrying " << fixed << setprecision(2) <<  MaxFuel << " Gallons of fuel" << endl;
+
     return 0;
 }
